cde7e6430c88ce37387b06f93efa25263338c86c
hanhuafeng <799954772@qq.com>
2020-11-18 22:52:18 +0800

    取栈中的最小值，时间复杂度0(1)

--- Class3/Code02_StackGetMin/src/hfhan/StackGetMin.java
+++ Class3/Code02_StackGetMin/src/hfhan/StackGetMin.java
@@ -39,7 +39,7 @@ public class StackGetMin {
 
     public int getMin(){
         if(pushStack.empty()){
-            throw new RuntimeException("栈还是空的!1");
+            throw new RuntimeException("栈还是空的!");
         }
         return this.minStack.peek();
     }
